501e7d1524e7876ab30225000621dc7806e89593
Caleb Currie <caleb@sele.dev>
2025-12-03 22:02:51 -0600

    feat: day 2 part 2 solved

--- aoc/2025/day_02/src/main.rs
+++ aoc/2025/day_02/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use itertools::*;
 use prse::parse;
 use std::fs::read_to_string;
 use util::get_inputs_filepath;
@@ -27,10 +28,9 @@ fn read_ranges(filename: &str) -> Result<Vec<Range>> {
     return res;
 }
 
-
-fn is_invalid(id: i64) ->  bool {
+fn is_invalid_p1(id: i64) -> bool {
     let id_str = id.to_string();
-    let id_len = id_str.len();
+    let id_len = id_str.chars().count();
     if id_len % 2 != 0 {
         // Can't have an invalid pattern if we're not of even length.
         return false;
@@ -40,24 +40,64 @@ fn is_invalid(id: i64) ->  bool {
     }
 }
 
+// Invalid IDs are only comprised of repeating groups.
+fn is_invalid_p2(id: i64) -> bool {
+    let id_str = id.to_string();
+    let id_len = id_str.chars().count();
+
+    let mut invalid = false;
+    for group_size in (1..id_len).step_by(1) {
+        let initial_group = id_str.get(0..group_size).unwrap();
+        invalid = id_str
+            .chars()
+            .chunks(group_size)
+            .into_iter()
+            .all(|group| group.collect::<String>() == initial_group);
+        if invalid {
+            break;
+        };
+    }
+    return invalid;
+}
+
 fn main() -> Result<()> {
     let input_filepath = get_inputs_filepath("day_02.txt");
     let ranges: Vec<Range> = read_ranges(input_filepath.as_str())?;
 
     // Find and add up invalid IDs
-    let invalids_total = ranges.iter().fold(0, |total, range| {
-        let sum = (range.start..=range.end).into_iter().fold(0, |elem_total, elem| {
-            if is_invalid(elem) {
-                elem_total + elem
-            } else {
-                elem_total + 0
-            }
-        });
-        total + sum
+    let invalids_total_p1 = ranges.iter().fold(0, |final_total, range| {
+        let sum = (range.start..=range.end)
+            .into_iter()
+            .fold(0, |range_total, elem| {
+                if is_invalid_p1(elem) {
+                    range_total + elem
+                } else {
+                    range_total + 0
+                }
+            });
+        final_total + sum
+    });
+
+    let invalids_total_p2 = ranges.iter().fold(0, |final_total, range| {
+        let sum = (range.start..=range.end)
+            .into_iter()
+            .fold(0, |range_total, elem| {
+                // println!("p2 testing id: {}", elem);
+                if is_invalid_p2(elem) {
+                    // println!("p2 invalid id: {}", elem);
+                    range_total + elem
+                } else {
+                    range_total + 0
+                }
+            });
+        final_total + sum
     });
 
     println!("## AoC 2025 Day 02 - Part 1");
-    println!("accumulated invalid IDs: {}", invalids_total);
+    println!("accumulated invalid IDs: {}", invalids_total_p1);
+
+    println!("## AoC 2025 Day 02 - Part 2");
+    println!("accumulated invalid IDs: {}", invalids_total_p2);
 
     Ok(())
 }
